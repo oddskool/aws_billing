a3c025c6eaa4031072133b1ae0efac3d33b5a435
eustache <eustache@diemert.fr>
2013-10-01 09:11:59 +0200

    raise exception when remote file not present

--- aws_billing/driver.py
+++ aws_billing/driver.py
@@ -9,21 +9,18 @@ from datetime import datetime
 import zipfile
 import os.path
 from boto import connect_s3
-from aws_billing.parser import parse
+
 
 def retrieve_fd(account, bucket_name, month=None, tmp_dir='.'):
     month = month or datetime.now().strftime('%Y-%m')
-    fn = "%s-aws-billing-detailed-line-items-with-resources-and-tags-%s.csv"%(account, month)
+    fn = "%s-aws-billing-detailed-line-items-with-resources-and-tags-%s.csv" % (account, month)
+    remote_fn = "s3://%s/%s.zip" % (bucket_name, fn)
+    print "remote fn:", remote_fn
     s3 = connect_s3()
     bucket = s3.get_bucket(bucket_name)
     key = bucket.get_key(fn+'.zip')
+    if not key:
+        raise Exception("remote file not ready : %s" % remote_fn)
     key.get_contents_to_filename(os.path.join(tmp_dir, fn+'.zip'))
-    return zipfile.ZipFile(os.path.join(tmp_dir,fn+'.zip')).open(fn)
+    return zipfile.ZipFile(os.path.join(tmp_dir, fn+'.zip')).open(fn)
 
-if __name__ == '__main__':
-    fd = retrieve_fd()
-    stats = parse(fd)
-    for d in stats:
-        print "%s : $%.2f " % (d[0],
-                               d[1]['Cost'])#,
-                                   #unitize(d[0], d[1]['UsageQuantity']))
--- aws_billing/server.py
+++ aws_billing/server.py
@@ -5,14 +5,15 @@ Created on 20 sept. 2013
 @author: ediemert
 '''
 
-import cyclone.web
 import sys
+import time
 
+import cyclone.web
 from twisted.internet import reactor
 from twisted.python import log
+
 from aws_billing.driver import retrieve_fd
 from aws_billing.parser import parse
-import time
 
 
 class MainHandler(cyclone.web.RequestHandler):
@@ -37,12 +38,12 @@ class MainHandler(cyclone.web.RequestHandler):
         self.write("<h2>Tag Based Billing Report</h2><table><tr><th>Billing Tag</th><th>Cost ($)</th></tr>")
         for d in self.application.stats:
             if 'BILLING' in d[0]:
-                self.write("<tr><td>%s</td><td>%.2f</td></tr>"%(d[0][10:],d[1]['Cost']))
+                self.write("<tr><td>%s</td><td>%.2f</td></tr>" % (d[0][10:],d[1]['Cost']))
         self.write("</table>")
         self.write("<h2>Resource Type Based Billing Report</h2><table><tr><th>Billing Tag</th><th>Cost ($)</th></tr>")
         for d in self.application.stats:
             if 'BILLING' not in d[0]:
-                self.write("<tr><td>%s</td><td>%.2f</td></tr>"%(d[0],d[1]['Cost']))
+                self.write("<tr><td>%s</td><td>%.2f</td></tr>" % (d[0],d[1]['Cost']))
         self.write("</table>")
         self.write("</p><tiny><a href='https://github.com/oddskool/aws_billing'>Fork me on Github !</a></tiny></p>")
 
